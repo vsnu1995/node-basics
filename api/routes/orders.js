edd6742f04b7c4ccce582ba81f6929c5d3436824
vsnu1995 <svishnu.1995@gmail.com>
2019-03-07 20:00:50 +0530

    Added order routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,26 +3,26 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: "GET request for products"
+        message: "GET request for orders"
     });
 });
 
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
     res.status(200).json({
-        message: `GET request for product with id ${id}`
+        message: `GET request for orders ${id}`
     });
 });
 
 router.post('/', (req, res, next) => {
     res.status(201).json({
-        message: "POST request for products"
+        message: "POST request for orders"
     });
 });
 
-router.patch('/', (req, res, next) => {
+router.delete('/', (req, res, next) => {
     res.status(200).json({
-        message: "PATCH request for products"
+        message: "DELETE request for orders"
     });
 });
 
